1c812adb2bddc49b4542f13365b2c014cde0dfa5
amercader <amercadero@gmail.com>
2022-11-09 23:29:51 +0100

    Try to use a separate setup file

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+pip install --user -r requirements.txt
+pip install --user -r dev-requirements.txt
+python setup.py develop --user
+ckan generate config ckan.ini
+mkdir /workspace/data
+ckan config-tool ckan.storage_path=/workspace/data
+ckan db init
+ckan user add ckan_admin email=admin@example.com password=test1234
+ckan sysadmin add ckan_admin
