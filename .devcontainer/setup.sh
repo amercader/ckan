19a39badab6e92a632917eff109008f0e2d12393
amercader <amercadero@gmail.com>
2022-11-25 15:33:49 +0100

    Revert "Try xloader"
    This reverts commit edc038a48aff8757c9cd917e23ba6e15b66946d3.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -18,16 +18,11 @@ ckan db init
 ckan user add ckan_admin email=admin@example.com password=test1234
 ckan sysadmin add ckan_admin
 
-# Set up DataStore
+# Set up DataStore + DataPusher
+ckan config-tool ckan.ini "ckan.datapusher.api_token=$(ckan user token add ckan_admin datapusher | tail -n 1 | tr -d '\t')"
 ckan config-tool ckan.ini \
     ckan.datastore.write_url=postgresql://ckan_default:pass@localhost/datastore_default \
     ckan.datastore.read_url=postgresql://datastore_default:pass@localhost/datastore_default \
-    "ckan.plugins=activity datastore datatables_view"
+    ckan.datapusher.url=http://localhost:8800 \
+    "ckan.plugins=activity datastore datapusher datatables_view"
 ckan datastore set-permissions | psql $(grep ckan.datastore.write_url ckan.ini | awk -F= '{print $2}')
-
-# Install ckanext-xloader
-git clone https://github.com/ckan/ckanext-xloader.git
-cd ckanext-xloader && python setup.py develop --user && cd ..
-pip install -r ckanext-xloader/requirements.txt
-ckan config-tool ckan.ini "ckan.xloader.api_token=$(ckan user token add ckan_admin datapusher | tail -n 1 | tr -d '\t')"
-ckan config-tool ckan.ini "ckan.plugins=activity datastore datatables_view xloader"
